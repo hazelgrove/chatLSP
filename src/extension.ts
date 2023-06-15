e93012103d048ef2dae53c24f29341a8e5fb007c
disconcision <me@andrewblinn.com>
2023-06-15 19:07:33 -0400

    reinit from solidjs vscode components starter

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,11 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { commands, ExtensionContext } from "vscode";
+import { ChatLSPPanel } from "./panels/ChatLSPPanel";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext) {
+  const showChatLSPCommand = commands.registerCommand("hello-world.showHelloWorld", () => {
+    ChatLSPPanel.render(context.extensionUri);
+  });
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "chatlsp" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('chatlsp.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('FML!!!! ChatLSP!');
-	});
-
-	context.subscriptions.push(disposable);
+  // Add command to the extension context
+  context.subscriptions.push(showChatLSPCommand);
 }
-
-// This method is called when your extension is deactivated
-export function deactivate() {}
